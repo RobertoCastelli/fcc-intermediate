2ccc35d1ae622d3f881269e91aeac7f0a4eb6592
akaro <roberto.castelli.teal@gmail.com>
2020-01-28 16:04:31 +0100

    added code

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let solutions = [
         title: "02 - Diff Two Arrays",
         description: "Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.",
         tester: function () { diffArr() },
-        code: "code 2"
+        code: "\ncode 2"
     },
     {
         title: "03 - Seek and Destroy",
