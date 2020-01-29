4804c1f0cccd71b89ac4c9247f021df16d2550dd
akaro <roberto.castelli.teal@gmail.com>
2020-01-29 13:51:38 +0100

    adding solutions

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// GLOBAL VARIABLES
 let title = document.getElementById('title')
 let description = document.getElementById('description')
 let test = document.getElementById('test')
@@ -5,38 +6,41 @@ let code = document.getElementById('code')
 let list = document.querySelectorAll('#list-view')
 let btnTop = document.getElementById('btnTop')
 
+// LIST OF SOLUTIONS 
 let solutions = [
     {
         title: "01 - Sum All Numbers",
         description: "We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.",
-        tester: function () { sumAll() },
-        code: "\nfunction sumAll(arr) { \n var max = Math.max(arr[0], arr[1]); \n var min = Math.min(arr[0], arr[1]); \n var temp = 0; \n for(var i = min; i <= max; i++) { \n temp += i; \n} \n return temp; \n} "
+        code: "\nfunction sumArr(arr) { \n let max = Math.max(arr[0], arr[1]); \n let min = Math.min(arr[0], arr[1]); \n let sum = 0; \n for(let i = min; i <= max; i++) { \n sum += i; \n } \n return sum; \n} "
     },
     {
         title: "02 - Diff Two Arrays",
         description: "Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.",
-        tester: function () { diffArr() },
-        code: "\ncode 2"
+        code: "\nfunction diffArray(arr1, arr2) { \n let newArr = [...arr1, ...arr2] \n return newArr.filter(e => \n  !arr1.includes(e) || !arr2.includes(e)\n )\n}"
     },
     {
         title: "03 - Seek and Destroy",
         description: "You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.",
-        tester: function () { seekDestroy() },
         code: "code 3"
     }
 ]
 
+// DISPLAY SOLUTION 
 list.forEach(element => {
-    element.addEventListener('click', (e) => {
+    element.addEventListener('click', e => {
+        scrollToTitle();
         let index = e.target.id
         title.innerHTML = solutions[index].title
         description.innerHTML = solutions[index].description
-        solutions[index].tester()
         code.innerHTML = solutions[index].code
     })
 })
 
-btnTop.addEventListener('click', () => document.documentElement.scrollTop = 0)
+// SCROLL TOP
+btnTop.addEventListener('click', () => document.documentElement.scrollTo({ top: 0, behavior: 'smooth' }))
 
-
+// SCROLL TO TITLE
+function scrollToTitle() {
+    title.scrollIntoView({ behavior: 'smooth' })
+}
 
--- functions.js
+++
@@ -1,3 +0,0 @@
-let sumAll = () => console.log('1')
-let diffArr = () => console.log('2')
-let seekDestroy = () => console.log('3')
