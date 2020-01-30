1fec40acf7a510fcdea267cfb25d690cd10f9631
RobertoCastelli <roberto.castelli.teal@gmail.com>
2020-01-30 22:24:02 +0100

    added pig latin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let solutions = [
     {
         title: "01 - Sum All Numbers",
         description: "We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.",
-        code: "\nfunction sumArr(arr) { \n let max = Math.max(arr[0], arr[1]); \n let min = Math.min(arr[0], arr[1]); \n let sum = 0; \n for(let i = min; i <= max; i++) { \n sum += i; \n } \n return sum; \n} "
+        code: "\nfunction sumArr(arr) { \n let max = Math.max(arr[0], arr[1]); \n let min = Math.min(arr[0], arr[1]); \n let sum = 0; \n for(let i = min; i <= max; i++) { \n  sum += i; \n } \n return sum; \n} "
     },
     {
         title: "02 - Diff Two Arrays",
@@ -21,12 +21,12 @@ let solutions = [
     {
         title: "03 - Seek and Destroy",
         description: "You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.",
-        code: "code 3"
+        code: "solving..."
     },
     {
         title: "04 - Wherefore art thou",
         description: "Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.",
-        code: "code 4"
+        code: "solving..."
     },
     {
         title: "05 - Spinal Tap Case",
@@ -36,7 +36,12 @@ let solutions = [
     {
         title: "06 - Pig Latin",
         description: "Translate the provided string to pig latin. Pig Latin takes the first consonant(or consonant cluster) of an English word, moves it to the end of the word and suffixes an 'ay'. If a word begins with a vowel you just add 'way' to the end. If a word does not contain a vowel, just add 'ay' to the end. Input strings are guaranteed to be English words in all lowercase.",
-        code: "code 6"
+        code: "\nfunction translatePigLatin(str) { \n let firstChar = /^[aeiou]/gi \n if(firstChar.test(str)) return str = str + 'way' \n\n let char = str.match(/^[^aeiou]+/gi) \n str = str.replace(char, '') \n return str + char + 'ay' \n}"
+    },
+    {
+        title: "07 - Search and Replace",
+        description: "Perform a search and replace on the sentence using the arguments provided and return the new sentence. First argument is the sentence to perform the search and replace on. Second argument is the word that you will be replacing (before). Third argument is what you will be replacing the second argument with (after).",
+        code: "solving..."
     }
 ]
 
