98f5e0eec4163c3adc0770e449f10ea2f5f6e22e
akaro <roberto.castelli.teal@gmail.com>
2020-01-30 14:57:04 +0100

    added spineCase

--- app.js
+++ app.js
@@ -22,6 +22,21 @@ let solutions = [
         title: "03 - Seek and Destroy",
         description: "You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.",
         code: "code 3"
+    },
+    {
+        title: "04 - Wherefore art thou",
+        description: "Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.",
+        code: "code 4"
+    },
+    {
+        title: "05 - Spinal Tap Case",
+        description: "Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.",
+        code: "\nfunction spinalCase(str) { \n return str \n  .replace(/([a-z])([A-Z])/g, '$1 $2') \n  .replace(/\s|_/g, '-') \n  .toLowerCase() \n}"
+    },
+    {
+        title: "06 - Pig Latin",
+        description: "Translate the provided string to pig latin. Pig Latin takes the first consonant(or consonant cluster) of an English word, moves it to the end of the word and suffixes an 'ay'. If a word begins with a vowel you just add 'way' to the end. If a word does not contain a vowel, just add 'ay' to the end. Input strings are guaranteed to be English words in all lowercase.",
+        code: "code 6"
     }
 ]
 
